792c0e867ac60c3272d6d6404cb8cc869d60c2c9
Emily <emili061116@gmail.com>
2024-06-25 15:24:36 +0300

    added profile

---
+++ controlllers/profControllers.js
@@ -0,0 +1,81 @@
+const PostModel = require("../models/Post");
+
+const getProfile = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const username = req.params.username;
+    if (username) {
+      res.render("prof/profile", { username, userId });
+    } else {
+      res.status(400).send("Username is required");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getPosts = async (req, res) => {
+  const userId = req.query.userId;
+  try {
+    const posts = await PostModel.find({ userId: userId });
+    if (!posts.length) {
+      console.log("There aren't posts");
+    }
+    res.status(200).json({ posts });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+const postPost = async (req, res) => {
+  const userId = req.user && req.user._id;
+  if (!userId) {
+    return res.status(400).send("User ID is required");
+  }
+
+  try {
+    const { title, text } = req.body;
+
+    const post = new PostModel({
+      title,
+      text,
+      userId: userId,
+    });
+    const newPost = await post.save();
+    res.status(200).json(newPost);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, text } = req.body;
+    const updatePost = await PostModel.findByIdAndUpdate(id, { title, text });
+    res.status(200).json(updatePost);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletePost = await PostModel.findByIdAndDelete(id);
+    if (!deletePost) {
+      return res.status(401).json({ message: "Post not found" });
+    }
+    res.status(200).json({ message: "Post deleted" });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+module.exports = {
+  getProfile,
+  getPosts,
+  postPost,
+  updatePost,
+  deletePost,
+};
